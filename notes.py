05685d9440595dca8cafbb4387242b6b15af5a4e
tom oneil <tomoneill500@gmail.com>
2024-06-25 14:55:42 +0100

    v1

---
+++ notes.py
@@ -0,0 +1,11 @@
+# ORM and OOP 
+
+#toms nots in said code.
+
+# what to do in my flask app / todo list maker.
+
+# make a edit text.id with todo.
+# error handleing maike id not able to type the same task with out an error just retern a message!
+# sort out css and handleing the todo site.
+# make the web more usert frendly.
+# make the header more better. with infomation. 
